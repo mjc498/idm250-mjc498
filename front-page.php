e46099a1b345dffbb0ccaf9582a9fc0b39ba6acf
Maria-Louisa Ching <146354776+mjc498@users.noreply.github.com>
2025-03-20 14:07:06 -0400

    Update front-page.php

--- front-page.php
+++ front-page.php
@@ -12,6 +12,10 @@
     <?php get_template_part('components/hero-home'); ?> 
 </section>
 
+<section class="logo-maria">
+    <img src="<?php echo get_template_directory_uri(); ?>/images/logo-maria.png" alt="Logo">
+</section>
+
 <section class="projects-section">
     <?php get_template_part('components/latest-projects'); ?> 
 </section>
