60e757cbd85aa2a72f05e85e83d318785426410d
Maria-Louisa Ching <146354776+mjc498@users.noreply.github.com>
2025-03-19 20:07:35 -0400

    Update 404.php

--- 404.php
+++ 404.php
@@ -1,10 +1,23 @@
+<?php
+/**
+ * 404 Page Template
+ *
+ * @package IDM250
+ */
+?>
+
 <?php get_header(); ?>
 
 <main class="error-page">
     <div class="container">
-        <h1>404 Page Not Found</h1>
-        <p>Sorry, we can’t find the page you’re looking for :(</p>
-        <a href="<?php echo home_url(); ?>" class="btn">Go Back Home</a>
+        <h1>404 - Page Not Found</h1>
+        <p class="error-message">
+            Oops! The page you’re looking for doesn’t exist.
+        </p>
+
+        <div class="error-options">
+            <a href="<?php echo home_url(); ?>" class="btn">Return to Homepage</a>
+        </div>
     </div>
 </main>
 
